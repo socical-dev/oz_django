84ec52f305b132a6a7e61e7f64f7421ab1e11506
socical <socical.dev@gmail.com>
2025-03-11 01:16:53 +0900

    🌱 Django의 Model, Field, Migrations 학습
    어드민 관리하는 것도 학습

--- day2/config/settings.py
+++ day2/config/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+OWN_APPS = [
+    'todo',
+]
+
+INSTALLED_APPS = DJANGO_APPS + OWN_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- day2/config/urls.py
+++ day2/config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import todo_list, todo_info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
 ]
