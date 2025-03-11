0fa41d2729f8fb6cc5756af96a3959351c2272a7
socical <socical.dev@gmail.com>
2025-03-11 23:56:28 +0900

    🐛 Day2의 todo 에 접근 못하는 이슈 발생
    day2.todo.view 는 day2/manage.py 를 실행할 때만 유효한 import 이기에 별도로 day3 에 추가해야되는 상황

--- day3/config/urls.py
+++ day3/config/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from day2.todo.views import todo_list, todo_info
+from day3.users import views as users_views
 
 urlpatterns = [
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', users_views.login, name='login'),
+    path('accounts/signup/', users_views.sign_up, name='signup')
 ]
